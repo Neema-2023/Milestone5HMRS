895cd7d1f996cf5d18815c79f93313f2d555c018
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-02 21:22:46 -0400

    Update home.php

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ $logger->info('User accessed home.php');
 
 echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
 echo '<div class="container mt-5">';
-echo '<h1>Welcome to the HR Management System</h1>';
+echo '<h1>Welcome to the CST323HR Management System</h1>';
 echo '<div class="list-group mt-3">';
 echo '<a href="employeemanagement.php" class="list-group-item list-group-item-action">Employee Management</a>';
 echo '<a href="departments.php" class="list-group-item list-group-item-action">Departments</a>';
