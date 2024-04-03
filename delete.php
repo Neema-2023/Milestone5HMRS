4751f6cb8165c443944d91be67cb2d56927960c0
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-02 21:29:08 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ $logger->info('User accessed delete.php');
 use App\Service\TrainingService;
 use App\Repository\TrainingScheduleRepository;
 
-$db = new mysqli('database-1.cpwgy466mtid.us-east-1.rds.amazonaws.com', 'admin', 'Zz8U&JP1^UczS', 'trainingDB');
+$db = new mysqli('milestone5hmrs-server.mysql.database.azure.com', 'gzfemdsgdy', '$XdsiMGt67QSoak2', 'milestone5hmrs-database');
 if ($db->connect_error) {
     $logger->info('Connection Failed at delete.php');
     die("Connection failed: " . $db->connect_error);
