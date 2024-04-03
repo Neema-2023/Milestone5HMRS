e895461302d1f98136e1006a700a934b691edec8
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-02 22:14:06 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,13 @@
+<?php
+$host = 'database-1.cfs8smgio0kv.us-east-2.rds.amazonaws.com';
+$dbname = 'database-1';
+$username = 'Neema';
+$password = 'Cleburne$$137';
 
-    <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:milestone5hmrs-server.mysql.database.azure.com; Database = milestone5hmrs-database", "gzfemdsgdy", "{$XdsiMGt67QSoak2}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+$conn = new mysqli($host, $username, $password, $dbname);
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "gzfemdsgdy", "pwd" => "{$XdsiMGt67QSoak2}", "Database" => "milestone5hmrs-database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:milestone5hmrs-server.mysql.database.azure.com.com";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+if ($conn->connect_error) {
+    $logger->info('Connection failed at db.php');
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
