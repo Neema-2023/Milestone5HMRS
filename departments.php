2927c107a82b21c9c44382600f071af410e0cce6
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-02 21:32:35 -0400

    Update departments.php

--- departments.php
+++ departments.php
@@ -8,7 +8,7 @@ $logger->info('User accessed department.php');
 use App\Service\TrainingService;
 use App\Repository\TrainingScheduleRepository;
 
-$db = new mysqli('milestone5hmrs-server.mysql.database.azure.com', 'gzfemdsgdy', '$XdsiMGt67QSoak2', 'milestone5hmrs-server');
+$db = new mysqli('milestone5hmrs-server.mysql.database.azure.com', 'gzfemdsgdy', '$XdsiMGt67QSoak2', 'milestone5hmrs-database');
 if ($db->connect_error) {
     $logger->info('Connection failed at departments.php');
     die("Connection failed: " . $db->connect_error);
