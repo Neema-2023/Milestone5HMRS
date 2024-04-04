4270fb36702f4bd56c8526783a8d5dbe022716ec
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-04 19:21:36 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
-$host = 'database-1.cfs8smgio0kv.us-east-2.rds.amazonaws.com';
-$dbname = 'database-1';
-$username = 'Neema';
-$password = 'Cleburne$$137';
-
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    $logger->info('Connection failed at db.php');
-    die("Connection failed: " . $conn->connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:neemalocalhost.database.windows.net,1433; Database = HMRS", "Neema", "{Cleburne@@137}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Neema", "pwd" => "{Cleburne@@137}", "Database" => "HMRS", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:neemalocalhost.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
