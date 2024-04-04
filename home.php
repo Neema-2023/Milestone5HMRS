894f82d9fb396007b18e0afcc68da3f63c4212c6
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-04 19:27:42 -0400

    Update home.php

--- home.php
+++ home.php
@@ -6,13 +6,13 @@ $logger = require_once __DIR__ . '/../config/bootstrap.php';
 
 $logger->info('User accessed home.php');
 
-echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
-echo '<div class="container mt-5">';
-echo '<h1>Welcome to the CST323HR Management System</h1>';
-echo '<div class="list-group mt-3">';
-echo '<a href="employeemanagement.php" class="list-group-item list-group-item-action">Employee Management</a>';
-echo '<a href="departments.php" class="list-group-item list-group-item-action">Departments</a>';
-echo '<a href="reports.php" class="list-group-item list-group-item-action">Training Reports</a>';
-echo '<a href="index.php" class="list-group-item list-group-item-action">Schedule a Class</a>';
-echo '</div></div>';
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<div class="container mt-5">
+<h1>Welcome to the CST323HR Management System</h1>
+<div class="list-group mt-3">
+<a href="employeemanagement.php" class="list-group-item list-group-item-action">Employee Management</a>
+<a href="departments.php" class="list-group-item list-group-item-action">Departments</a>
+<a href="reports.php" class="list-group-item list-group-item-action">Training Reports</a>
+<a href="index.php" class="list-group-item list-group-item-action">Schedule a Class</a>
+</div></div>'
 ?>
